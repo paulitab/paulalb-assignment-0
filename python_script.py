a984899498b8dce6b8509e2d01d223d2b081a780
Paula Burgos <paulalb@bu.edu>
2024-09-15 13:40:13 -0400

    adding the python script to assignment 0 repo

---
+++ python_script.py
@@ -0,0 +1,7 @@
+# dummy assignment
+# python script that adds two numbers together and prints their sum to the command line.
+def add(a, b):
+    return a + b
+
+if __name__ == '__main__':
+    print(add(2, 3))
